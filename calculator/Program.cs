cada66e71ec39931a1b2ae82074519ba5de767e2
Mickey Mouse <Mickey.Mouse@softwire.com>
2022-07-04 17:13:30 +0100

    Created simple calculation with the four operators.

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -6,11 +6,31 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter an operator: ");
+            string op = Console.ReadLine();
+            
+            Console.WriteLine("Please enter a number: ");
             string input1 = Console.ReadLine();
             double num1 = Double.Parse(input1);
+            Console.WriteLine("Please enter another number: ");
             string input2 = Console.ReadLine();
             double num2 = Double.Parse(input2);
-            double ans = num1 + num2;
+            double ans = 0;
+            switch (op)
+            {
+                case "+":
+                    ans = num1 + num2;
+                    break;
+                case "-":
+                    ans = num1 - num2; 
+                    break;
+                case "*":
+                    ans = num1 * num2;
+                    break;
+                case "/":
+                    ans = num1 / num2;
+                    break;
+            }
             Console.WriteLine(ans);
         }
     }
