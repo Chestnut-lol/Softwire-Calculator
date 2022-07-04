f0c4656758c5bf47c48bcac8682fc5167d26f33f
Mickey Mouse <Mickey.Mouse@softwire.com>
2022-07-04 17:00:08 +0100

    Add a simple multiplier

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -6,10 +6,12 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            string input = Console.ReadLine();
-            double radius = Double.Parse(input);
-            double area = Math.PI * (radius * radius);
-            Console.WriteLine(area);
+            string input1 = Console.ReadLine();
+            double num1 = Double.Parse(input1);
+            string input2 = Console.ReadLine();
+            double num2 = Double.Parse(input2);
+            double ans = num1 + num2;
+            Console.WriteLine(ans);
         }
     }
 }
