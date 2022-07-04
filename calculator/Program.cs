6f1f33368187f244589c08e80dbbfabf8b5f6b5b
Mickey Mouse <Mickey.Mouse@softwire.com>
2022-07-04 17:33:36 +0100

    Working on exercise 4.

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -8,29 +8,38 @@ namespace Calculator
         {
             Console.WriteLine("Please enter an operator: ");
             string op = Console.ReadLine();
-            
-            Console.WriteLine("Please enter a number: ");
-            string input1 = Console.ReadLine();
-            double num1 = Double.Parse(input1);
-            Console.WriteLine("Please enter another number: ");
-            string input2 = Console.ReadLine();
-            double num2 = Double.Parse(input2);
-            double ans = 0;
-            switch (op)
+            Console.WriteLine($"How many numbers do you want to {op}?");
+            int cnt = int.Parse(Console.ReadLine());
+            int[] nums = new int[cnt];
+            for (int i = 0; i < cnt; i++)
             {
-                case "+":
-                    ans = num1 + num2;
-                    break;
-                case "-":
-                    ans = num1 - num2; 
-                    break;
-                case "*":
-                    ans = num1 * num2;
-                    break;
-                case "/":
-                    ans = num1 / num2;
-                    break;
+                Console.WriteLine("Please enter a number: ");
+                string input = Console.ReadLine();
+                int num = int.Parse(input);
+                nums.Add(num);
             }
+            
+            int ans = 0;
+            foreach(int num in nums)
+            {
+                Console.WriteLine(num);
+                switch (op)
+                {
+                    case "+":
+                        ans = num + ans;
+                        continue;
+                    case "-":
+                        ans = num - ans; 
+                        continue;
+                    case "*":
+                        ans = num * ans;
+                        continue;
+                    case "/":
+                        ans = num / ans;
+                        continue;
+                }
+            }
+                
             Console.WriteLine(ans);
         }
     }
